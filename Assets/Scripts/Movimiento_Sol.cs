f5d2e17dd381398da04ab5f6ef26eca95c08cf3d
fenixblanco18 <montero.helena9@gmail.com>
2024-09-20 17:37:04 +0200

    Hud a medias

--- Assets/Scripts/Movimiento_Sol.cs
+++ Assets/Scripts/Movimiento_Sol.cs
@@ -7,12 +7,11 @@ public class Movimiento_Sol : MonoBehaviour
 {
     [Range(0.0f, 24f)] public float Hora = 12;
     public Transform Sol;
-    public Transform Luna;
     public float velocidadTransicion = 1.0f; // Velocidad de la transición día/noche
 
     private float SolX;
 
-    private float LunaX;
+  
 
     private void Update()
     {
@@ -28,7 +27,6 @@ public class Movimiento_Sol : MonoBehaviour
     {
         SolX = 15 * Hora;
         Sol.localEulerAngles = new Vector3(SolX, 0, 0);
-        LunaX = 15 * Hora;
-        Luna.localEulerAngles = new Vector3(LunaX, 0, 0);
     }
+
 }
